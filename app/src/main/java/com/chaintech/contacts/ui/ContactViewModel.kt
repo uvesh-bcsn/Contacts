a9e822e2ee58ac331bd8de2d9bfa0ba580b72709
uvesh-bcsn <uvesh.m@chaintech.network>
2024-10-17 17:45:17 +0530

    [Add] - Fetch contacts after 6 hours completed

--- app/src/main/java/com/chaintech/contacts/ui/ContactViewModel.kt
+++ app/src/main/java/com/chaintech/contacts/ui/ContactViewModel.kt
@@ -1,10 +1,12 @@
 package com.chaintech.contacts.ui
 
 import android.app.Application
+import android.util.Log
 import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.chaintech.contacts.ContactHelper.fetchContacts
+import com.chaintech.contacts.pref.PreferencesManager
 import com.chaintech.contacts.repository.ContactRepository
 import com.chaintech.contacts.room.table.Contact
 import dagger.hilt.android.lifecycle.HiltViewModel
@@ -12,7 +14,11 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
+import java.time.Duration
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 import javax.inject.Inject
+import kotlin.math.log
 
 @HiltViewModel
 class ContactViewModel @Inject constructor(
@@ -22,8 +28,20 @@ class ContactViewModel @Inject constructor(
 
     var contacts = mutableStateOf<List<Contact>>(emptyList())
 
+    val pref = PreferencesManager(app)
+
     init {
-        reFetch()
+        val newDate = LocalDateTime.now()
+        val defaultTime = newDate.minusHours(7)
+        val date = stringToLocalDateTime(pref.getData("fetchTime", localDateTimeToString(defaultTime)))
+        val duration = Duration.between(date, newDate).toHours()
+
+        if (duration > 6) {
+            pref.saveData("fetchTime", localDateTimeToString(LocalDateTime.now()))
+            reFetch()
+        } else {
+            readContact()
+        }
     }
 
     var isRefresh = mutableStateOf(false)
@@ -47,5 +65,15 @@ class ContactViewModel @Inject constructor(
         contactRepository.readContact().onEach {
             contacts.value = it
         }.launchIn(viewModelScope)
+    }
+
+    private fun localDateTimeToString(dateTime: LocalDateTime, pattern: String = "yyyy-MM-dd HH:mm:ss"): String {
+        val formatter = DateTimeFormatter.ofPattern(pattern)
+        return dateTime.format(formatter)
+    }
+
+    private fun stringToLocalDateTime(dateTimeString: String, pattern: String = "yyyy-MM-dd HH:mm:ss"): LocalDateTime {
+        val formatter = DateTimeFormatter.ofPattern(pattern)
+        return LocalDateTime.parse(dateTimeString, formatter)
     }
 }
